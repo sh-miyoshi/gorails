b6cca4199bf16261e2477eff1c79b87dc6ab1178
s.miyoshi <s.miyoshi1991@gmail.com>
2022-08-18 12:18:04 +0900

    generate model

--- pkg/cmd/generate.go
+++ pkg/cmd/generate.go
@@ -3,12 +3,21 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"strings"
+	"text/template"
 
+	"github.com/sh-miyoshi/gorails/pkg/cmd/util"
 	"github.com/spf13/cobra"
 )
 
+type Column struct {
+	Key   string
+	Value string
+}
+
 func init() {
 	rootCmd.AddCommand(generateCmd)
+	generateCmd.Flags().StringArray("columns", []string{}, "")
 }
 
 var generateCmd = &cobra.Command{
@@ -31,12 +40,71 @@ var generateCmd = &cobra.Command{
 		case "controller", "c":
 			fmt.Println("WIP")
 		case "model", "m":
-			fmt.Println("WIP")
+			// Create file
+			fname := fmt.Sprintf("app/models/%s.go", strings.ToLower(resName))
+			fp, err := os.Create(fname)
+			if err != nil {
+				fmt.Printf("Failed to create model file: %+v\n", err)
+				os.Exit(1)
+			}
+			defer fp.Close()
+
+			// Write model struct
+			tpl, err := template.New("").Parse(modelTemplate)
+			if err != nil {
+				fmt.Printf("System error. Failed to parse model template: %+v", err)
+				os.Exit(1)
+			}
+			data := struct {
+				ModelName string
+				Columns   []Column
+			}{
+				ModelName: strings.ToUpper(resName[:1]) + strings.ToLower(resName[1:]),
+				Columns:   parseColumns(cmd),
+			}
+			tpl.Execute(fp, data)
+			util.RunCommand("go", "fmt", fname)
+
+			fmt.Println("Successfully generate model")
 		case "view", "v":
 			fmt.Println("WIP")
 		default:
-			fmt.Printf("Invalid resource type %s is specified", resType)
+			fmt.Printf("Invalid resource type %s is specified\n", resType)
 			os.Exit(1)
 		}
 	},
 }
+
+func parseColumns(cmd *cobra.Command) []Column {
+	columns, _ := cmd.Flags().GetStringArray("columns")
+	res := []Column{}
+	for _, c := range columns {
+		d := strings.Split(c, ":")
+		if len(d) != 2 {
+			fmt.Printf("Invalid columns %s was speficied\n", c)
+			fmt.Println("--columns requires <Key>:<type> format")
+			os.Exit(1)
+		}
+		key := strings.TrimSpace(d[0])
+		key = strings.ToUpper(key[:1]) + strings.ToLower(key[1:])
+		res = append(res, Column{Key: key, Value: strings.TrimSpace(d[1])})
+	}
+
+	return res
+}
+
+var modelTemplate = `package models
+
+import "time"
+
+type {{ .ModelName }} struct {
+	ID        string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+
+{{ range .Columns }}
+	{{ .Key }} {{ .Value }}
+{{ end }}
+}
+
+`
