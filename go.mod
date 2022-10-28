8808397d2678e911f3d73a4986ce65688c347fee
s.miyoshi <s.miyoshi1991@gmail.com>
2022-10-28 20:02:41 +0900

    add build command

--- go.mod
+++ go.mod
@@ -22,5 +22,6 @@ require (
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/kr/pretty v0.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/stretchr/stew v0.0.0-20130812190256-80ef0842b48b
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 )
--- pkg/cmd/build.go
+++ pkg/cmd/build.go
@@ -1,11 +1,19 @@
 package cmd
 
 import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/sh-miyoshi/gorails/pkg/cmd/util"
 	"github.com/spf13/cobra"
+	"github.com/stretchr/stew/slice"
 )
 
 func init() {
 	rootCmd.AddCommand(buildCmd)
+	buildCmd.Flags().String("target", "all", "build target [all | separate]")
+	buildCmd.Flags().String("tag", "latest", "docker image tag")
 }
 
 var buildCmd = &cobra.Command{
@@ -13,15 +21,31 @@ var buildCmd = &cobra.Command{
 	Short: "Build docker images",
 	Long:  `Build docker images`,
 	Run: func(cmd *cobra.Command, args []string) {
-		/*
-			案
-				Dockerfileはnew時にコピーしておく
-				引数でallinoneかprd用を選択
-				docker build
+		targets := []string{"all", "separate"}
+		target, _ := cmd.Flags().GetString("target")
+		if !slice.Contains(targets, target) {
+			fmt.Printf("Invalid target type %s was specified\n", target)
+			os.Exit(1)
+		}
+		clientExists := util.FileExists("client")
+		if target == "all" && !clientExists {
+			fmt.Printf("[Warning] cannot find client directory, so will create server image only")
+			target = "separate"
+		}
+		project, err := os.ReadFile(".gorails-project")
+		if err != nil {
+			fmt.Printf("Failed to read .gorails-project file: %+v\n", err)
+			os.Exit(1)
+		}
+		tag, _ := cmd.Flags().GetString("tag")
+		image := strings.Trim(string(project), "\n") + ":" + tag
 
-				newコマンドのgo-mod-pathをproject-pathとかにする
-				それをファイルに書き込んでおく
-				image名は<project-path>/<project-name>:latestとか
-		*/
+		switch target {
+		case "all":
+			util.RunCommand("docker", "build", "-f", "build/Dockerfile.all", "-t", image, ".")
+		case "separate":
+			panic("TODO: WIP")
+			// TODO build server, build client if exists
+		}
 	},
 }
